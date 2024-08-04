ff0e7198107e5796fe9ec79c96ef1cd221cd4595
vaibha987 <v2goyal95@gmail.com>
2024-08-04 20:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+let activeCircle = 1;
+const totalCircles = 5;
+
+const circles = Array.from(document.querySelectorAll('.circle'));
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
+
+nextButton.addEventListener('click', () => {
+    circles[activeCircle - 1].classList.remove('active');
+    activeCircle += 1;
+    circles[activeCircle - 1].classList.add('active');
+
+    prevButton.disabled = false;
+
+    if (activeCircle === totalCircles) {
+        nextButton.disabled = true;
+    }
+});
+
+prevButton.addEventListener('click', () => {
+    circles[activeCircle - 1].classList.remove('active');
+    activeCircle -= 1;
+    circles[activeCircle - 1].classList.add('active');
+
+    nextButton.disabled = false;
+
+    if (activeCircle === 1) {
+        prevButton.disabled = true;
+    }
+});
